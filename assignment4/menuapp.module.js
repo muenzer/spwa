acc04f68f28b353fda93e434ab1f0de51f8fa2eb
Chris Muenzer <chris.muenzer.cm1@roche.com>
2016-10-12 12:32:17 +0200

    initial assignment 4

---
+++ assignment4/menuapp.module.js
@@ -0,0 +1,1 @@
+angular.module('MenuApp', ['data']);
---
+++ assignment4/menudata.service.js
@@ -0,0 +1,27 @@
+angular.module('data')
+.service('MenuDataService', MenuDataService)
+.constand('URL', 'https://davids-restaurant.herokuapp.com/');
+
+MenuDataService.$inject = ['URL', '$http'];
+function MenuDataService(URL, $http) {
+  var service = this;
+
+  service.getAllCategories = function () {
+    var response = $http({
+      method: "GET",
+      url: URL + "/categories.json"
+    });
+
+    return response;
+  };
+
+  service.getItemsForCategory = function (categoryShortName) {
+    var response = $http({
+      method: "GET",
+      url: URL + "/menu_items.json",
+      params: {category: categoryShortName}
+    });
+
+    return response;
+  }
+}
